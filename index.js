47ccc5b722da517380e4e19d74b58bfc6052bd1b
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-18 18:43:40 +0600

    mongodb and server is running successfully

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
-// require('dotenv').config()
+require('dotenv').config()
 
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-onjpk5k-shard-00-00.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-01.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-02.xskcn3u.mongodb.net:27017/?ssl=true&replicaSet=atlas-g07jbs-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req,res)=> {
     res.send('teddy bear server is running')
