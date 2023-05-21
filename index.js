799064ff1834cd7a92206c387e52273f89d5ccc4
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-21 18:04:57 +0600

    some information is added

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-onjpk5k-shard-00-00.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-01.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-02.xskcn3u.mongodb.net:27017/?ssl=true&replicaSet=atlas-g07jbs-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,126 +27,100 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const TeddyCollection = client.db("teddyDb").collection('teddy')
-
-    // const indexKeys = {name : 1}
-    // const indexOptions = {name : 'toyName'}
-
-    // const result = await TeddyCollection.createIndex(indexKeys,indexOptions)
+    const TeddyCollection = client.db("teddyDb").collection("teddy");
 
     app.get("/toySearch/:text", async (req, res) => {
       const text = req.params.text;
       const Alldata = await TeddyCollection.find().toArray();
-      const result = Alldata.filter((data) => data.name.toLowerCase().includes(text.toLowerCase()));
+      const result = Alldata.filter((data) =>
+        data.name.toLowerCase().includes(text.toLowerCase())
+      );
       res.send(result);
-   
-   
-    })
-   
-    // app.get('/toySearch/:text', async(req,res)=> {
+    });
 
-    //   const toySearch = req.params.text
-    //   const result = await TeddyCollection.find({
+    app.post("/allTeddy", async (req, res) => {
+      const newTeddy = req.body;
+      newTeddy.price = parseFloat(req.body.price)
+      console.log(newTeddy);
+      const result = await TeddyCollection.insertOne(newTeddy);
+      res.send(result);
+    });
 
-    //     $or : [
-    //       {name : {$regex : toySearch, $options : 'i'}},
-    //     ],
+    app.get("/allTeddy", async (req, res) => {
+      const result = await TeddyCollection.find().limit(20).toArray();
+      res.send(result);
+    });
 
-    //   }).toArray()
-
-    //   res.send(result)
-
-    // })
-
-
-
-    app.post('/allTeddy', async(req,res)=>{
-        const newTeddy = req.body 
-        console.log(newTeddy)
-        const result = await TeddyCollection.insertOne(newTeddy)
-        res.send(result)
-    })
-
-    app.get('/allTeddy', async(req,res)=> {
-        const result = await TeddyCollection.find().limit(20).toArray()
-        res.send(result)
-    })
-
-    app.get('/allTeddy/:id', async (req,res)=> {
-
-        const id = req.params.id
-        const query = {_id : new ObjectId(id)}
-        const result = await TeddyCollection.find(query).toArray()
-        res.send(result)
-
-    })
-
-   
+    app.get("/allTeddy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await TeddyCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // this query for getting data from email
 
-    app.get('/myTeddy/:email', async(req,res)=>{
+    app.get("/myTeddy/:email", async (req, res) => {
 
-      // const type = req.params.type === 'ascending'
-      // const sortObj = {}
-      // const value = req.params.value
-      //  sortObj[value] = type ? 1 : -1
-      // .sort(sortObj)
+      const sort = req.query.sort === 'ascending' ? 1 : -1;
 
-        const result = await TeddyCollection.find({email : req.params.email}).toArray()
-        res.send(result)
-    })
+      const result = await TeddyCollection.find({email: req.params.email,}).sort({price : sort}).toArray();
+      res.send(result);
+    
+    
+    });
 
-    // this query for getting data from email
+   
+   
+   
+    // this query for getting data from category
 
-    app.get('/newTeddy/:text', async(req,res)=> {
-
-      if(req.params.text == 'Honey Bear' || req.params.text == 'Snugglekins' || req.params.text == 'Fuzzy'){
-        const result = await TeddyCollection.find({Sub_Category : req.params.text}).toArray()
-        res.send(result)
+    app.get("/newTeddy/:text", async (req, res) => {
+      if (
+        req.params.text == "Honey Bear" ||
+        req.params.text == "Snugglekins" ||
+        req.params.text == "Fuzzy"
+      ) {
+        const result = await TeddyCollection.find({
+          Sub_Category: req.params.text,
+        }).toArray();
+        res.send(result);
       }
+    });
 
-       
-    })
-
-    
-    
     
     
     // it for update
-     app.put('/myTeddy/:id', async(req,res)=>{
-        const id = req.params.id 
-        const filter = {_id : new ObjectId(id)}
-        const options = {upsert : true}
-        const udpdatedTeddy = req.body
-        const teddy = {
-            $set : {
-                price:udpdatedTeddy.price,
-                Available_quantity: udpdatedTeddy.Available_quantity,
-                Detail_description: udpdatedTeddy.Detail_description,
-               }
-        }
-    
-        const result = await TeddyCollection.updateOne(filter,teddy,options)
-        res.send(result)
-    })
+    app.put("/myTeddy/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const udpdatedTeddy = req.body;
+      const teddy = {
+        $set: {
+          price: parseFloat(udpdatedTeddy.price),
+          Available_quantity: udpdatedTeddy.Available_quantity,
+          Detail_description: udpdatedTeddy.Detail_description,
+        },
+      };
 
+      const result = await TeddyCollection.updateOne(filter, teddy, options);
+      res.send(result);
+    });
 
+   
+   
+   
+   
     // its for delete
 
-    app.delete('/myTeddy/:id', async(req,res)=>{
-        const id = req.params.id 
-        const query = {_id : new ObjectId(id)}
-        const result = await TeddyCollection.deleteOne(query)
-        res.send(result)
-    })
+    app.delete("/myTeddy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await TeddyCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-
-
-
-   
-   
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -160,14 +131,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("teddy bear server is running");
+});
 
-
-
-
-app.get('/', (req,res)=> {
-    res.send('teddy bear server is running')
-})
-
-app.listen(port, ()=>{
-     console.log(`teddy bear server is running on port : ${port} `)
-})
+app.listen(port, () => {
+  console.log(`teddy bear server is running on port : ${port} `);
+});
